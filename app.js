4135cd6bd2143a1b33a2320ba2acbfd95c08ddc0
Marco Antonio <roam130602@gmail.com>
2025-08-15 16:45:04 -0600

    Funcion de enlistar los nombres ingresados

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-let amigoGenerado;
 
 function agregarAmigo(){
     //Creamos una variable para almacenar el nombre ingresado
-    amigoGenerado = document.getElementById("amigo").value;
+    let amigoGenerado = document.getElementById("amigo").value;
 
     //Si el programa detecta que en la variable no hay ningun nombre, nos indicará que ingresemos uno
     if (amigoGenerado.length == 0) {
         alert("Ingresa un nombre correcto");
-    } else {
+        enfocarInput();
+        return;
+
+    } 
+    if (amigos.includes(amigoGenerado)) {
+        alert(`${amigoGenerado} ya se encuentra en la lista`);
+        limpiarSeccion('amigo');
+        enfocarInput();
+        return;
+    }
         //Si el programa detectó que si se ingresó un nombre correcto, guardará el nombre en nuestro arreglo
         amigos.push(amigoGenerado);
-        document.getElementById("amigo").value = '';
+        limpiarSeccion("amigo");
+        enfocarInput();
+        actualizarLista();
     }
 
-    console.log(amigos)
+    // funcion para actualizar la lista de amigos
+    function actualizarLista() {
+        let listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.innerHTML = "";
+
+        for(let i = 0; i < amigos.length; i++) {
+            const li = document.createElement("li");
+            li.textContent = amigos[i];
+            listaAmigos.appendChild(li);
+        }
+    }
+
+function limpiarSeccion(identificador){
+    document.getElementById(identificador).value = '';
+    return;
+}
+
+function enfocarInput(){
+    document.getElementById("amigo").focus();
 }
